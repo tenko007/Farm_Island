7e2f2c8b946b8466c40e53fec4c055097538b0e0
tenko007 <tenko2007@gmail.com>
2022-01-29 13:45:24 +0300

    Popups now blocks buttons on main UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,6 +9,8 @@ namespace UI
 {
     public class UIManager : MonoBehaviour, IUIManager 
     {
+        [Header("Temp prefabs")]
+        [SerializeField] private BaseModel houseModel;
         [Header("Main UI")] 
 
         [Header("Popups")]
@@ -18,10 +20,15 @@ namespace UI
         [SerializeField] private GameObject MainCanvas;
         [SerializeField] private GameObject NotificationsCanvas;
         [SerializeField] private GameObject PopupsCanvas;
+
+        [Header("Buttons")] 
+        [SerializeField] private GameObject BuildButton;
         private void Start()
         {
             Services.GetService<IBuildingSystem>().OnBuildingStart +=
                 _ => Instantiate(buildingActions.gameObject, PopupsCanvas.transform);
+            BuildButton.GetComponent<Button>().onClick.AddListener(() => Services.GetService<IBuildingSystem>()
+                .SetModelToBuild(houseModel).StartBuild());
         }
 
     }
